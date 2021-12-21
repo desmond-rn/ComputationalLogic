3dea5fd3acc4753f5b5c9706d3993c39a00de06e
RDN (Mac) <desmond.ngueguin@gmail.com>
2021-12-21 16:10:35 +0000

    Download nltk

--- prolexa/meta_grammar.py
+++ prolexa/meta_grammar.py
@@ -6,6 +6,9 @@ import string
 from enum import Enum
 from flair.data import Sentence
 from flair.models import SequenceTagger
+
+import nltk
+nltk.download()
 from nltk.stem import WordNetLemmatizer
 
 from prolexa import PACKAGE_PATH, PROLOG_PATH
